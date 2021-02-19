fdd0fc1e899a59a91431c7921e0059cd3f59e0b3
ysl2 <songli.yu@outlook.com>
2021-02-19 23:42:15 +0800

    2021-02-19 23:42:15

---
+++ src/servlet/ServletDemo01.java
@@ -0,0 +1,38 @@
+package src.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.*;
+
+/**
+ * Servlet快速入门
+ */
+public class ServletDemo01 implements Servlet {
+
+
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    //提供服务的方法
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println("Hello Servlet");
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
